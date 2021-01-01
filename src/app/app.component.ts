08a1e5411da09f865675419f0675262c89df5f0d
Amith-B <37371517+Amith-B@users.noreply.github.com>
2021-01-01 16:28:38 +0530

    added responsive mat drawer which auto closes when scren size reduces to sm or xs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { PageheadingService } from './services/pageheading.service';
 import { portfolioDataInterface, PortfolioinfoService } from './services/portfolioinfo.service';
+import {MediaChange, MediaObserver} from '@angular/flex-layout';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,41 +12,47 @@ import { portfolioDataInterface, PortfolioinfoService } from './services/portfol
 export class AppComponent implements OnInit, OnDestroy{
 
   profileInfoLoaded:Boolean=false;
+  title = 'Portfolio';
+  sidebarToggle=true;
+  mobileDevice=false;
 
-  constructor(private portfolioinfoService:PortfolioinfoService){
+  watcher: Subscription;
+  activeMediaQuery = '';
+
+  constructor(
+    private portfolioinfoService:PortfolioinfoService,
+    mediaObserver: MediaObserver){
+
+    this.watcher = mediaObserver.media$.subscribe((change: MediaChange) => {
+      this.activeMediaQuery = change ? `'${change.mqAlias}' = (${change.mediaQuery})` : '';
+      if ( change.mqAlias == 'xs' || change.mqAlias == 'sm') {
+         this.sidebarToggle=false;
+         this.mobileDevice=true;
+      }
+      else {
+        this.sidebarToggle=true;
+        this.mobileDevice=false;
+     }
+    });
 
   }
   ngOnDestroy(): void {
-  
+    this.watcher.unsubscribe();
   }
 
   ngOnInit(): void {
-    // this.portfolioinfoService.parseProfileInfo().then(()=>{
-    //   console.log('Success');
-    //   this.profileInfoLoaded=true;
-    // }).catch((error)=>{
-    //   console.log('Failure');
-    //   this.profileInfoLoaded=true;
-    // });
-
+    
     console.log("getting profile details....")
     this.portfolioinfoService.parseProfileInfo()
         .subscribe((data)=>{
           this.portfolioinfoService.setProfileInfo(data);
         })
+
   }
-  title = 'Portfolio';
-
-  sidebarToggle=true;
-
-
+  
   
   toggleSideBar(){
     this.sidebarToggle=!this.sidebarToggle
   }
-
-  // setToolbarHeading(name){
-  //   this.toolbarHeading=name;
-  // }
 
 }

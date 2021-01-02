04a4e7700d8f2c7226d14ea8f5e676f53b43d029
Amith-B <37371517+Amith-B@users.noreply.github.com>
2021-01-02 22:33:05 +0530

    added angular animation to underline component

--- src/app/shared/component/underline/underline.component.ts
+++ src/app/shared/component/underline/underline.component.ts
@@ -1,9 +1,24 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-underline',
   templateUrl: './underline.component.html',
-  styleUrls: ['./underline.component.scss']
+  styleUrls: ['./underline.component.scss'],
+  animations: [
+    trigger('bar1Slide',[
+      transition(':enter', [
+        style({transform: 'translateX(-120px)'}),
+        animate('.4s .1s ease-in-out', style({transform: 'translateX(0px)'}))
+      ])
+    ]),
+    trigger('bar2Slide',[
+      transition(':enter', [
+        style({transform: 'translateX(-100px)'}),
+        animate('.2s .3s ease-in-out', style({transform: 'translateX(0px)'}))
+      ])
+    ])
+  ]
 })
 export class UnderlineComponent implements OnInit {
 

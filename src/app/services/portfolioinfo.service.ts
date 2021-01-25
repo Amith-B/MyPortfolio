6d098814c7ee251c5037e37e0acad4d536f3de63
Amith-B <37371517+Amith-B@users.noreply.github.com>
2021-01-25 23:21:54 +0530

    removed the gif and made a custom animation

--- src/app/services/portfolioinfo.service.ts
+++ src/app/services/portfolioinfo.service.ts
@@ -5,191 +5,171 @@ import * as portfolioInfo from './../../assets/portfolioInfo.json';
 
 export interface dataInterface {
   info: {
-    dob: string,
-    degree:string,
-    dept:string,
-    city:string,
-    phone:string,
-    passout:string,
-    about_heading:string,
-    about_para:string,
-    address:{
-      line1:string,
-      line2:string
-    },
-    email:string,
-    profileURL:string,
-    facebook:string,
-    linkedin:string,
-    github:string,
-  },
-  eduction:[
+    dob: string;
+    degree: string;
+    dept: string;
+    city: string;
+    phone: string;
+    passout: string;
+    about_heading: string;
+    about_para: string;
+    address: {
+      line1: string;
+      line2: string;
+    };
+    email: string;
+    profileURL: string;
+    facebook: string;
+    linkedin: string;
+    github: string;
+  };
+  eduction: [
     {
-      date_range:string,
-      heading:string,
-      description:string,
-      percentage:string
+      date_range: string;
+      heading: string;
+      description: string;
+      percentage: string;
     }
-  ],
-  experience:[
+  ];
+  experience: [
     {
-      date_range:string,
-      designation:string,
-      company:string,
-      description: [
-        string
-      ],
+      date_range: string;
+      designation: string;
+      company: string;
+      description: [string];
     }
-  ],
-  knowledge:[
+  ];
+  knowledge: [
     {
-      name:string,
-      percentage: number
+      name: string;
+      percentage: number;
     }
-  ]
+  ];
 }
 
 export interface portfolioDataInterface {
-  imageURL:string,
-  cardTitle:string,
-  cardSubTitle:string,
-  description:string,
-  projectURL:string
+  imageURL: string;
+  cardTitle: string;
+  cardSubTitle: string;
+  description: string;
+  projectURL: string;
 }
 
 export interface messageInterface {
   name: string;
   mail: string;
-  subject: string; 
+  subject: string;
   message: string;
 }
 
 export interface visitCountInterface {
-  newvisit:number;
-  revisit:number;
+  newvisit: number;
+  revisit: number;
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PortfolioinfoService {
-  
-  profileDataChangeEvent:EventEmitter<dataInterface>=new EventEmitter();
-  visitCount:EventEmitter<visitCountInterface>=new EventEmitter();
+  profileDataChangeEvent: EventEmitter<dataInterface> = new EventEmitter();
+  visitCount: EventEmitter<visitCountInterface> = new EventEmitter();
 
-  firebaseURL="https://amith-portfolio-default-rtdb.firebaseio.com"
+  firebaseURL = 'https://amith-portfolio-default-rtdb.firebaseio.com';
 
-  myInfo:dataInterface=(profileInfo as any).default;
-  
+  myInfo: dataInterface = (profileInfo as any).default;
+
   portfolioData: Array<portfolioDataInterface> = (portfolioInfo as any).default;
 
-  newvisit=0;
-  revisit=0;
+  newvisit = 0;
+  revisit = 0;
 
-  constructor(private http:HttpClient) { 
+  constructor(private http: HttpClient) {}
 
-  }
-
-  parseProfileInfo(){
-   
-    return this.http
-            .get<dataInterface>(
-                `${this.firebaseURL}/profile.json`,
-                {
-                    responseType:'json'
-                }
-            )
-  }
-
-  parsePorfolioData(){
-    return this.http
-            .get<Array<portfolioDataInterface>>(
-                `${this.firebaseURL}/portfolio.json`,
-                {
-                    responseType:'json'
-                }
-            )
-
-  }
-
-  setPorfolioData(data:Array<portfolioDataInterface>){
-    this.portfolioData=data;
-  }
-
-  setProfileInfo(data:dataInterface){
-    this.myInfo=data;
-    this.profileDataChangeEvent.emit(data);
-  }
-
-  getPorfolioData(){
-    return this.portfolioData;
-  }
-
-  getProfileInfo(){
-    return this.myInfo
-  }
-
-  pushMessages(data:messageInterface){
-    return this.http.post(
-        `${this.firebaseURL}/messages.json`,
-        data
-      );
-  }
-
-  private fetchCounter(path:string){
-    return this.http.get(
-      `${this.firebaseURL}/${path}.json`
-    );
-  }
-
-  private incrementCounter(counter:number,path:string){
-    this.http.put(
-      `${this.firebaseURL}/${path}.json`,
-      {counter:counter+1}
-    ).subscribe((data)=>{
-      console.log('Counter increment successfull');
-      this[path]+=1;
-
-      this.getVisit();
-
-    },(error)=>{
-      console.log('Unable to increment counter right now');
-      this.getVisit();
-    })
-  }
-
-  setAsUniqueUser(){
-    console.log('New user')
-    this.fetchCounter('newvisit').subscribe((data:{counter:number})=>{
-      this.newvisit=data.counter;
-      this.incrementCounter(this.newvisit,'newvisit');
-    })
-  }
-
-  setAsRevisitedUser(){
-    console.log('Previously visited user, welcome back')
-    this.fetchCounter('revisit').subscribe((data:{counter:number})=>{
-      this.revisit=data.counter;
-      this.incrementCounter(this.revisit,'revisit');
-    })
-  }
-
-  getVisit(){
-    this.fetchCounter('newvisit').subscribe((data:{counter:number})=>{
-      this.newvisit=data.counter;
-
-      this.fetchCounter('revisit').subscribe((data:{counter:number})=>{
-        this.revisit=data.counter;
-
-        this.visitCount.emit(
-          {
-            newvisit: this.newvisit,
-            revisit: this.revisit
-          }
-        )
-      })
-
+  parseProfileInfo() {
+    return this.http.get<dataInterface>(`${this.firebaseURL}/profile.json`, {
+      responseType: 'json',
     });
   }
 
+  parsePorfolioData() {
+    return this.http.get<Array<portfolioDataInterface>>(
+      `${this.firebaseURL}/portfolio.json`,
+      {
+        responseType: 'json',
+      }
+    );
+  }
+
+  setPorfolioData(data: Array<portfolioDataInterface>) {
+    this.portfolioData = data;
+  }
+
+  setProfileInfo(data: dataInterface) {
+    this.myInfo = data;
+    this.profileDataChangeEvent.emit(data);
+  }
+
+  getPorfolioData() {
+    return this.portfolioData;
+  }
+
+  getProfileInfo() {
+    return this.myInfo;
+  }
+
+  pushMessages(data: messageInterface) {
+    return this.http.post(`${this.firebaseURL}/messages.json`, data);
+  }
+
+  private fetchCounter(path: string) {
+    return this.http.get(`${this.firebaseURL}/${path}.json`);
+  }
+
+  private incrementCounter(counter: number, path: string) {
+    this.http
+      .put(`${this.firebaseURL}/${path}.json`, { counter: counter + 1 })
+      .subscribe(
+        (data) => {
+          console.log('Counter increment successfull');
+          this[path] += 1;
+
+          this.getVisit();
+        },
+        (error) => {
+          console.log('Unable to increment counter right now');
+          this.getVisit();
+        }
+      );
+  }
+
+  setAsUniqueUser() {
+    console.log('New user');
+    this.fetchCounter('newvisit').subscribe((data: { counter: number }) => {
+      this.newvisit = data.counter;
+      this.incrementCounter(this.newvisit, 'newvisit');
+    });
+  }
+
+  setAsRevisitedUser() {
+    console.log('Previously visited user, welcome back');
+    this.fetchCounter('revisit').subscribe((data: { counter: number }) => {
+      this.revisit = data.counter;
+      this.incrementCounter(this.revisit, 'revisit');
+    });
+  }
+
+  getVisit() {
+    this.fetchCounter('newvisit').subscribe((data: { counter: number }) => {
+      this.newvisit = data.counter;
+
+      this.fetchCounter('revisit').subscribe((data: { counter: number }) => {
+        this.revisit = data.counter;
+
+        this.visitCount.emit({
+          newvisit: this.newvisit,
+          revisit: this.revisit,
+        });
+      });
+    });
+  }
 }
